a01edce83893997b7c43c4d38bb2e49860501289
Gudelfina <gudelfina.mendez@gmail.com>
2023-01-23 16:22:19 -0700

    Feature 5 complete

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
+def project_list(request):
+    project = Project.objects.all()
+    context = {
+        "projects": project,
+    }
+    return render(request, "projects/list.html", context)
