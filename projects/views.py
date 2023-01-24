703ac0083ca243ca2fe93bef8c6a27a6eb86b6b6
Gudelfina <gudelfina.mendez@gmail.com>
2023-01-23 19:21:36 -0700

    Feature 13 complete

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from projects.models import Project
+from tasks.models import Task
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -10,3 +11,12 @@ def project_list(request):
         "projects": project,
     }
     return render(request, "projects/list.html", context)
+
+
+@login_required
+def show_project(request, id):
+    project = Project.objects.get(id=id)
+    context = {
+        "project_object": project,
+    }
+    return render(request, "projects/detail.html", context)
